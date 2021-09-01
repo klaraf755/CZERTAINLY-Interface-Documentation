f912bfb7bc923587d5ed15ee7830bb8d800542e2
David Svamberk <david.svamberk@gmail.com>
2021-09-01 11:53:00 +0200

    DTOs reorganized

--- doc-openapi-ca-connector/src/main/java/company/threekey/raprofiles/api/impl/EndEntityProfilesControllerDummyImpl.java
+++ doc-openapi-ca-connector/src/main/java/company/threekey/raprofiles/api/impl/EndEntityProfilesControllerDummyImpl.java
@@ -2,7 +2,7 @@ package company.threekey.raprofiles.api.impl;
 
 import company.threekey.raprofiles.api.exception.NotFoundException;
 import company.threekey.raprofiles.api.interfaces.EndEntityProfilesController;
-import company.threekey.raprofiles.api.model.NameAndIdDto;
+import company.threekey.raprofiles.api.model.ca.NameAndIdDto;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
