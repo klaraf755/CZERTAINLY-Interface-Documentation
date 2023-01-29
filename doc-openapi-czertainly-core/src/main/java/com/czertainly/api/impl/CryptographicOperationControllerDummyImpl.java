a34644b1831756ebb0036821ce6b27b27696528d
Pradeep Saminathan <76426163+3KeyPradeep@users.noreply.github.com>
2023-01-29 21:21:08 +0530

    Update cryptographic operation implementation documentation

--- doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/CryptographicOperationControllerDummyImpl.java
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/CryptographicOperationControllerDummyImpl.java
@@ -2,19 +2,16 @@ package com.czertainly.api.impl;
 
 import com.czertainly.api.exception.ConnectorException;
 import com.czertainly.api.interfaces.core.web.CryptographicOperationsController;
-import com.czertainly.api.model.client.cryptography.operations.CipherDataRequestDto;
-import com.czertainly.api.model.client.cryptography.operations.RandomDataRequestDto;
-import com.czertainly.api.model.client.cryptography.operations.SignDataRequestDto;
-import com.czertainly.api.model.client.cryptography.operations.VerifyDataRequestDto;
+import com.czertainly.api.model.client.cryptography.operations.*;
 import com.czertainly.api.model.common.attribute.v2.BaseAttribute;
 import com.czertainly.api.model.connector.cryptography.enums.CryptographicAlgorithm;
-import com.czertainly.api.model.connector.cryptography.operations.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class CryptographicOperationControllerDummyImpl implements CryptographicOperationsController {
+
 
     @Override
     public List<BaseAttribute> listCipherAttributes(String tokenInstanceUuid, String tokenProfileUuid, String uuid, String keyItemUuid, CryptographicAlgorithm algorithm) throws ConnectorException {
