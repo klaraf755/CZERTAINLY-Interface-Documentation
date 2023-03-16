ed355d59d7702035afbcc4a1e3897c3acd3a4af7
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2023-03-16 07:45:46 +0100

    Update settings specification

--- doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
@@ -1,38 +1,20 @@
 package com.czertainly.api.impl;
 
-import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.interfaces.core.web.SettingController;
-import com.czertainly.api.model.client.attribute.RequestAttributeDto;
-import com.czertainly.api.model.common.attribute.v2.BaseAttribute;
-import com.czertainly.api.model.core.setting.AllSettingsDto;
-import com.czertainly.api.model.core.setting.Section;
-import com.czertainly.api.model.core.setting.SectionDto;
-import com.czertainly.api.model.core.setting.SectionSettingsDto;
+import com.czertainly.api.model.core.settings.PlatformSettingsDto;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class SettingControllerDummyImpl implements SettingController {
 
     @Override
-    public List<SectionDto> getSections() {
+    public PlatformSettingsDto getPlatformSettings() {
         return null;
     }
 
     @Override
-    public AllSettingsDto getAllSettings() {
-        return null;
+    public void updatePlatformSettings(PlatformSettingsDto platformSettingsDto) {
+
     }
 
-    @Override
-    public List<SectionSettingsDto> getSettings() {
-        return null;
-    }
-
-    @Override
-    public List<SectionSettingsDto> updateSettings(Map<Section, List<RequestAttributeDto>> attributes) {
-        return null;
-    }
 }
