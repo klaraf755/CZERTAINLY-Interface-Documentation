fbf9a69953a16eb6267d845400ee9a91c6cd9aa9
moro-lukasrejha <lukas.rejha@morosystems.cz>
2023-06-09 14:13:16 +0200

    CZERTAINLY-Core_#460 Fix enable/disable operation to reflect result of operation in Scheduler service

--- doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/DiscoveryControllerDummyImpl.java
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/DiscoveryControllerDummyImpl.java
@@ -9,9 +9,8 @@ import com.czertainly.api.model.client.certificate.SearchRequestDto;
 import com.czertainly.api.model.client.discovery.DiscoveryCertificateResponseDto;
 import com.czertainly.api.model.client.discovery.DiscoveryDto;
 import com.czertainly.api.model.client.discovery.DiscoveryHistoryDetailDto;
-import com.czertainly.api.model.core.scheduler.SchedulerJobInfoDto;
+import com.czertainly.api.model.core.scheduler.ScheduleDiscoveryDto;
 import com.czertainly.api.model.core.search.SearchFieldDataByGroupDto;
-import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,7 +56,7 @@ public class DiscoveryControllerDummyImpl implements DiscoveryController {
     }
 
     @Override
-    public void scheduleDiscovery(SchedulerJobInfoDto schedulerJobInfo, DiscoveryDto request) throws AlreadyExistException, CertificateException, InterruptedException, ConnectorException {
+    public void scheduleDiscovery(ScheduleDiscoveryDto scheduleDiscoveryDto) throws AlreadyExistException, CertificateException, InterruptedException, ConnectorException {
 
     }
 }
