f6455c8dd9fca5d446f386bcab44e64cb062aa22
lubomirw <76479559+lubomirw@users.noreply.github.com>
2023-07-20 11:43:24 +0200

    Add endpoints for managing approvals (#45)

--- doc-openapi-core-ra-profile/src/main/java/com/czertainly/api/impl/RAProfileManagementControllerDummyImpl.java
+++ doc-openapi-core-ra-profile/src/main/java/com/czertainly/api/impl/RAProfileManagementControllerDummyImpl.java
@@ -5,10 +5,12 @@ import com.czertainly.api.exception.ConnectorException;
 import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.exception.ValidationException;
 import com.czertainly.api.interfaces.core.web.RAProfileManagementController;
+import com.czertainly.api.model.client.approvalprofile.ApprovalProfileResponseDto;
 import com.czertainly.api.model.client.compliance.SimplifiedComplianceProfileDto;
 import com.czertainly.api.model.client.raprofile.*;
 import com.czertainly.api.model.common.attribute.v2.BaseAttribute;
 import com.czertainly.api.model.core.raprofile.RaProfileDto;
+import com.czertainly.api.model.core.scheduler.PaginationRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -125,5 +127,20 @@ public class RAProfileManagementControllerDummyImpl implements RAProfileManageme
     @Override
     public List<SimplifiedComplianceProfileDto> getAssociatedComplianceProfiles(String authorityUuid, String raProfileUuid) throws NotFoundException {
         return null;
+    }
+
+    @Override
+    public ApprovalProfileResponseDto getAssociatedApprovalProfile(String authorityUuid, String raProfileUuid, PaginationRequestDto paginationRequestDto) {
+        return null;
+    }
+
+    @Override
+    public void associateRAProfileWithApprovalProfile(String authorityUuid, String raProfileUuid, String approvalProfileUuid) throws NotFoundException {
+
+    }
+
+    @Override
+    public void disassociateRAProfileFromApprovalProfile(String authorityUuid, String raProfileUuid, String approvalProfileUuid) throws NotFoundException {
+
     }
 }
---
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/ApprovalControllerDummyImpl.java
@@ -0,0 +1,50 @@
+package com.czertainly.api.impl;
+
+import com.czertainly.api.exception.NotFoundException;
+import com.czertainly.api.exception.ValidationException;
+import com.czertainly.api.interfaces.core.web.ApprovalController;
+import com.czertainly.api.model.client.approval.ApprovalDetailDto;
+import com.czertainly.api.model.client.approval.ApprovalResponseDto;
+import com.czertainly.api.model.client.approval.ApprovalUserDto;
+import com.czertainly.api.model.client.approval.UserApprovalDto;
+import com.czertainly.api.model.core.scheduler.PaginationRequestDto;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApprovalControllerDummyImpl implements ApprovalController {
+
+    @Override
+    public ApprovalResponseDto listApprovals(PaginationRequestDto paginationRequestDto) throws ValidationException {
+        return null;
+    }
+
+    @Override
+    public ApprovalResponseDto listUserApprovals(PaginationRequestDto paginationRequestDto, ApprovalUserDto approvalUserDto) throws ValidationException {
+        return null;
+    }
+
+    @Override
+    public ApprovalDetailDto getApproval(String uuid) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void approveApproval(String uuid) throws NotFoundException {
+
+    }
+
+    @Override
+    public void rejectApproval(String uuid) throws NotFoundException {
+
+    }
+
+    @Override
+    public void approveApprovalRecipient(String uuid, UserApprovalDto userApprovalDto) throws NotFoundException {
+
+    }
+
+    @Override
+    public void rejectApprovalRecipient(String uuid, UserApprovalDto userApprovalDto) throws NotFoundException {
+
+    }
+}
