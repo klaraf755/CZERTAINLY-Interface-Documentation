cd383c2e34a8d17bbfcf60334ad1e50c630dfd76
dmaixner-moro <116064373+dmaixner-moro@users.noreply.github.com>
2023-07-24 14:05:31 +0200

    Implement management of notification instances (#46)

--- doc-openapi-core-notification-instance/src/main/java/com/czertainly/api/impl/NotificationInstanceControllerDummyImpl.java
+++ doc-openapi-core-notification-instance/src/main/java/com/czertainly/api/impl/NotificationInstanceControllerDummyImpl.java
@@ -41,7 +41,7 @@ public class NotificationInstanceControllerDummyImpl implements NotificationInst
     }
 
     @Override
-    public List<DataAttribute> listMappingAttributes(String kind) {
+    public List<DataAttribute> listMappingAttributes(String connectorUuid, String kind) throws ConnectorException {
         return null;
     }
 }
