4b770089510a1f3f9715b87e397ed8972bb97b5d
lubomirw <76479559+lubomirw@users.noreply.github.com>
2023-07-31 16:59:17 +0200

    Add notifications settings endpoints (#47)

--- doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
@@ -1,6 +1,7 @@
 package com.czertainly.api.impl;
 
 import com.czertainly.api.interfaces.core.web.SettingController;
+import com.czertainly.api.model.core.settings.NotificationSettingsDto;
 import com.czertainly.api.model.core.settings.PlatformSettingsDto;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,16 @@ public class SettingControllerDummyImpl implements SettingController {
 
     @Override
     public void updatePlatformSettings(PlatformSettingsDto platformSettingsDto) {
+
+    }
+
+    @Override
+    public NotificationSettingsDto getNotificationsSettings() {
+        return null;
+    }
+
+    @Override
+    public void updateNotificationsSettings(NotificationSettingsDto notificationSettingsDto) {
 
     }
 
