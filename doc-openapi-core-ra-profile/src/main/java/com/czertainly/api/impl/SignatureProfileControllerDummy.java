17505e5f7b70a94ec80cb9928c13917ae775913b
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-02-28 10:58:46 +0100

    Update SignatureProfileControllerDummy.java

--- doc-openapi-core-ra-profile/src/main/java/com/czertainly/api/impl/SignatureProfileControllerDummy.java
+++ doc-openapi-core-ra-profile/src/main/java/com/czertainly/api/impl/SignatureProfileControllerDummy.java
@@ -1,7 +1,9 @@
 package com.czertainly.api.impl;
 
 import com.czertainly.api.interfaces.core.web.SignatureProfileController;
+import com.czertainly.api.model.client.attribute.ResponseAttributeDto;
 import com.czertainly.api.model.core.signature.SignatureProfileDto;
+import com.czertainly.api.model.core.signature.SignatureProfileRequestDto;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,6 +12,31 @@ import java.util.List;
 public class SignatureProfileControllerDummy implements SignatureProfileController {
     @Override
     public List<SignatureProfileDto> listSignatureProfiles() {
+        return null;
+    }
+
+    @Override
+    public SignatureProfileDto createSignatureProfile(SignatureProfileRequestDto signatureProfileRequestDto) {
+        return null;
+    }
+
+    @Override
+    public SignatureProfileDto getSignatureProfileDetails(String signatureProfileUuid) {
+        return null;
+    }
+
+    @Override
+    public SignatureProfileDto updateSignatureProfile(String signatureProfileUuid, SignatureProfileRequestDto signatureProfileRequestDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteSignatureProfile(String signatureProfileUuid) {
+
+    }
+
+    @Override
+    public List<ResponseAttributeDto> listSignatureProfileAttributes(String signatureProfileUuid) {
         return null;
     }
 }
