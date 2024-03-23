a0522da56919fcad8a5e8631fe1e09bdad098e4f
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2024-03-23 14:17:29 +0100

    Add RulesController to whole CZERTAINLY Core module API

---
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/RulesControllerDummyImpl.java
@@ -0,0 +1,112 @@
+package com.czertainly.api.impl;
+
+import com.czertainly.api.exception.NotFoundException;
+import com.czertainly.api.interfaces.core.web.RuleController;
+import com.czertainly.api.model.core.auth.Resource;
+import com.czertainly.api.model.core.rules.*;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RulesControllerDummyImpl implements RuleController {
+    @Override
+    public List<RuleDto> listRules(Resource resource) {
+        return null;
+    }
+
+    @Override
+    public RuleDetailDto createRule(RuleRequestDto request) {
+        return null;
+    }
+
+    @Override
+    public RuleDetailDto getRule(String ruleUuid) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public RuleDetailDto updateRule(String ruleUuid, UpdateRuleRequestDto request) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteRule(String ruleUuid) throws NotFoundException {
+
+    }
+
+    @Override
+    public List<RuleConditionGroupDto> listConditionGroups(Resource resource) {
+        return null;
+    }
+
+    @Override
+    public RuleConditionGroupDetailDto createConditionGroup(RuleConditionGroupRequestDto request) {
+        return null;
+    }
+
+    @Override
+    public RuleConditionGroupDetailDto getConditionGroup(String conditionGroupUuid) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public RuleConditionGroupDetailDto updateConditionGroup(String conditionGroupUuid, UpdateRuleConditionGroupRequestDto request) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteConditionGroup(String conditionGroupUuid) throws NotFoundException {
+
+    }
+
+    @Override
+    public List<RuleActionGroupDto> listActionGroups(Resource resource) {
+        return null;
+    }
+
+    @Override
+    public RuleActionGroupDetailDto createActionGroup(RuleActionGroupRequestDto request) {
+        return null;
+    }
+
+    @Override
+    public RuleActionGroupDetailDto getActionGroup(String actionGroupUuid) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public RuleActionGroupDetailDto updateActionGroup(String actionGroupUuid, UpdateRuleActionGroupRequestDto request) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteActionGroup(String actionGroupUuid) throws NotFoundException {
+
+    }
+
+    @Override
+    public List<RuleTriggerDto> listTriggers(Resource resource, Resource triggerResource) {
+        return null;
+    }
+
+    @Override
+    public RuleTriggerDetailDto createTrigger(RuleTriggerRequestDto request) {
+        return null;
+    }
+
+    @Override
+    public RuleTriggerDetailDto getTrigger(String triggerUuid) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public RuleTriggerDetailDto updateTrigger(String triggerUuid, UpdateRuleTriggerRequestDto request) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteTrigger(String triggerUuid) throws NotFoundException {
+
+    }
+}
