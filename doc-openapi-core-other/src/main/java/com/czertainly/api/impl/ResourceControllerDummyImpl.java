c654d9b4c2309a9d1422ca604ee2be0cabe61914
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-04-22 19:24:49 +0200

    Add endpoint for listing events by resource (#74)

--- doc-openapi-core-other/src/main/java/com/czertainly/api/impl/ResourceControllerDummyImpl.java
+++ doc-openapi-core-other/src/main/java/com/czertainly/api/impl/ResourceControllerDummyImpl.java
@@ -3,6 +3,7 @@ package com.czertainly.api.impl;
 import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.interfaces.core.web.ResourceController;
 import com.czertainly.api.model.core.auth.Resource;
+import com.czertainly.api.model.core.rules.ResourceEvent;
 import com.czertainly.api.model.core.search.SearchFieldDataByGroupDto;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ import java.util.List;
 public class ResourceControllerDummyImpl implements ResourceController {
     @Override
     public List<SearchFieldDataByGroupDto> listResourceRuleFilterFields(Resource resource, boolean settable) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<ResourceEvent> listResourceEvents(Resource resource) {
         return null;
     }
 }
