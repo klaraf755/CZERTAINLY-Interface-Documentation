0f0744ce2dc3ef07e7624568880b606d64dda9c9
lubomirw <76479559+lubomirw@users.noreply.github.com>
2024-05-09 08:56:57 +0200

    Add resource listing and update resource event listing response (#76)

--- doc-openapi-core-other/src/main/java/com/czertainly/api/impl/ResourceControllerDummyImpl.java
+++ doc-openapi-core-other/src/main/java/com/czertainly/api/impl/ResourceControllerDummyImpl.java
@@ -3,7 +3,8 @@ package com.czertainly.api.impl;
 import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.interfaces.core.web.ResourceController;
 import com.czertainly.api.model.core.auth.Resource;
-import com.czertainly.api.model.core.rules.ResourceEvent;
+import com.czertainly.api.model.core.other.ResourceDto;
+import com.czertainly.api.model.core.other.ResourceEventDto;
 import com.czertainly.api.model.core.search.SearchFieldDataByGroupDto;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,19 @@ import java.util.List;
 
 @RestController
 public class ResourceControllerDummyImpl implements ResourceController {
+
     @Override
-    public List<SearchFieldDataByGroupDto> listResourceRuleFilterFields(Resource resource, boolean settable) throws NotFoundException {
-        return null;
+    public List<ResourceDto> listResources() {
+        return List.of();
     }
 
     @Override
-    public List<ResourceEvent> listResourceEvents(Resource resource) {
-        return null;
+    public List<SearchFieldDataByGroupDto> listResourceRuleFilterFields(Resource resource, boolean settable) throws NotFoundException {
+        return List.of();
+    }
+
+    @Override
+    public List<ResourceEventDto> listResourceEvents(Resource resource) {
+        return List.of();
     }
 }
--- doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/ResourceControllerDummyImpl.java
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/ResourceControllerDummyImpl.java
@@ -3,7 +3,9 @@ package com.czertainly.api.impl;
 import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.interfaces.core.web.ResourceController;
 import com.czertainly.api.model.core.auth.Resource;
-import com.czertainly.api.model.core.rules.ResourceEvent;
+import com.czertainly.api.model.core.other.ResourceDto;
+import com.czertainly.api.model.core.other.ResourceEvent;
+import com.czertainly.api.model.core.other.ResourceEventDto;
 import com.czertainly.api.model.core.search.SearchFieldDataByGroupDto;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,19 @@ import java.util.List;
 
 @RestController
 public class ResourceControllerDummyImpl implements ResourceController {
+
     @Override
-    public List<SearchFieldDataByGroupDto> listResourceRuleFilterFields(Resource resource, boolean settable) throws NotFoundException {
-        return null;
+    public List<ResourceDto> listResources() {
+        return List.of();
     }
 
     @Override
-    public List<ResourceEvent> listResourceEvents(Resource resource) {
-        return null;
+    public List<SearchFieldDataByGroupDto> listResourceRuleFilterFields(Resource resource, boolean settable) throws NotFoundException {
+        return List.of();
+    }
+
+    @Override
+    public List<ResourceEventDto> listResourceEvents(Resource resource) {
+        return List.of();
     }
 }
