6205ab08d02014bacacd4744ff28b586a25fc612
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-07-23 10:29:09 +0200

    Add endpoint for updating scheduled job (#83)

--- doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SchedulerControllerDummyImpl.java
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SchedulerControllerDummyImpl.java
@@ -1,11 +1,13 @@
 package com.czertainly.api.impl;
 
 import com.czertainly.api.exception.NotFoundException;
+import com.czertainly.api.exception.SchedulerException;
 import com.czertainly.api.interfaces.core.web.SchedulerController;
 import com.czertainly.api.model.core.scheduler.PaginationRequestDto;
 import com.czertainly.api.model.core.scheduler.ScheduledJobDetailDto;
 import com.czertainly.api.model.core.scheduler.ScheduledJobHistoryResponseDto;
 import com.czertainly.api.model.core.scheduler.ScheduledJobsResponseDto;
+import com.czertainly.api.model.scheduler.UpdateScheduledJob;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class SchedulerControllerDummyImpl implements SchedulerController {
 
     @Override
     public ScheduledJobDetailDto getScheduledJobDetail(String uuid) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public ScheduledJobDetailDto updateScheduledJob(String uuid, UpdateScheduledJob request) throws NotFoundException, SchedulerException {
         return null;
     }
 
