6292e7c9696468e3ab93598727df87c799b5933c
3keyroman <46850604+3keyroman@users.noreply.github.com>
2024-11-29 08:52:31 +0100

    Update settings docs with OAuth2 and logging (#95)

--- doc-openapi-core-other/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
+++ doc-openapi-core-other/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
@@ -1,8 +1,9 @@
 package com.czertainly.api.impl;
 
 import com.czertainly.api.interfaces.core.web.SettingController;
-import com.czertainly.api.model.core.settings.NotificationSettingsDto;
-import com.czertainly.api.model.core.settings.PlatformSettingsDto;
+import com.czertainly.api.model.core.settings.*;
+import com.czertainly.api.model.core.settings.logging.LoggingSettingsDto;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,5 +29,39 @@ public class SettingControllerDummyImpl implements SettingController {
 
     }
 
+    @Override
+    public AuthenticationSettingsDto getAuthenticationSettings() {
+        return null;
+    }
+
+    @Override
+    public void updateAuthenticationSettings(AuthenticationSettingsUpdateDto authenticationSettingsDto) {
+
+    }
+
+    @Override
+    public OAuth2ProviderSettingsDto getOAuth2ProviderSettings(String providerName) {
+        return null;
+    }
+
+    @Override
+    public void updateOAuth2ProviderSettings(String providerName, @Valid OAuth2ProviderSettingsDto oauth2SettingsDto) {
+
+    }
+
+    @Override
+    public void removeOAuth2Provider(String providerName) {
+
+    }
+
+    @Override
+    public LoggingSettingsDto getLoggingSettings() {
+        return null;
+    }
+
+    @Override
+    public void updateLoggingSettings(LoggingSettingsDto loggingSettingsDto) {
+
+    }
 
 }
--- doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
+++ doc-openapi-czertainly-core/src/main/java/com/czertainly/api/impl/SettingControllerDummyImpl.java
@@ -1,13 +1,14 @@
 package com.czertainly.api.impl;
 
 import com.czertainly.api.interfaces.core.web.SettingController;
-import com.czertainly.api.model.core.settings.NotificationSettingsDto;
-import com.czertainly.api.model.core.settings.PlatformSettingsDto;
+import com.czertainly.api.model.core.settings.*;
+import com.czertainly.api.model.core.settings.logging.LoggingSettingsDto;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SettingControllerDummyImpl implements SettingController {
-
+    
     @Override
     public PlatformSettingsDto getPlatformSettings() {
         return null;
@@ -25,6 +26,41 @@ public class SettingControllerDummyImpl implements SettingController {
 
     @Override
     public void updateNotificationsSettings(NotificationSettingsDto notificationSettingsDto) {
+
+    }
+
+    @Override
+    public AuthenticationSettingsDto getAuthenticationSettings() {
+        return null;
+    }
+
+    @Override
+    public void updateAuthenticationSettings(AuthenticationSettingsUpdateDto authenticationSettingsDto) {
+
+    }
+
+    @Override
+    public OAuth2ProviderSettingsDto getOAuth2ProviderSettings(String providerName) {
+        return null;
+    }
+
+    @Override
+    public void updateOAuth2ProviderSettings(String providerName, @Valid OAuth2ProviderSettingsDto oauth2SettingsDto) {
+
+    }
+
+    @Override
+    public void removeOAuth2Provider(String providerName) {
+
+    }
+
+    @Override
+    public LoggingSettingsDto getLoggingSettings() {
+        return null;
+    }
+
+    @Override
+    public void updateLoggingSettings(LoggingSettingsDto loggingSettingsDto) {
 
     }
 
